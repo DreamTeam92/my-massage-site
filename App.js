cc45f928c50a90584c757f4d47e1cd33c33b009c
VIKTOR ARVA-TOTH <45672316+DreamTeam92@users.noreply.github.com>
2024-10-30 16:52:40 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import ServiceList from './components/ServiceList';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <ServiceList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

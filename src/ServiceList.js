fafa5431f42d7c3cd980fae6d9f5adadefd119fe
VIKTOR ARVA-TOTH <45672316+DreamTeam92@users.noreply.github.com>
2024-10-30 16:56:54 +0000

    Create ServiceList.js

---
+++ src/ServiceList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ServiceList = () => {
+  return (
+    <section className="services">
+      <h2>Our Services</h2>
+      <ul>
+        <li>Deep Tissue Massage</li>
+        <li>Swedish Massage</li>
+        <li>Hot Stone Therapy</li>
+      </ul>
+    </section>
+  );
+};
+
+export default ServiceList;
